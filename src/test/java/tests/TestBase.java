e5f0e0cc3f1fcd22370db19800c78f42377c8caf
Marina Konovalchik <marina200014@yandex.ru>
2023-04-13 21:19:45 +0300

    added Owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,35 +2,57 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.ProjectConfiguration;
+import config.WebConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RabotaByPage;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class TestBase {
-        RabotaByPage rabotaByPage = new RabotaByPage();
+
+    private static WebConfig config;
+    private static ProjectConfiguration configuration;
+
+    RabotaByPage rabotaByPage = new RabotaByPage();
 
         @BeforeAll
         static void beforeAll() {
-            Configuration.browser = System.getProperty("browser", "chrome");
-            Configuration.browserVersion = System.getProperty("browserVersion", "100");
-            Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-            Configuration.timeout = 10000;
-            Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-            Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+     //       Configuration.browser = System.getProperty("browser", "chrome");
+     //       Configuration.browserVersion = System.getProperty("browserVersion", "100");
+     //       Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+    //        Configuration.timeout = 10000;
+    //        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+    //        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
+    //        DesiredCapabilities capabilities = new DesiredCapabilities();
+    //        capabilities.setCapability("enableVNC", true);
+    //        capabilities.setCapability("enableVideo", true);
+    //        Configuration.browserCapabilities = capabilities;
+
+
+                config = ConfigFactory.create(WebConfig.class, System.getProperties());
+                configuration = new ProjectConfiguration();
+                configuration.webConfig(config);
+
+                Configuration.pageLoadTimeout = 10000;
+                Configuration.timeout = 10000;
+                Configuration.headless = false;
+                Configuration.holdBrowserOpen = false;
         }
 
         @BeforeEach
         void addListener(){
-            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+     //       SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+
+                SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+                open("https://rabota.by");
         }
 
         @AfterEach
@@ -39,6 +61,7 @@ public class TestBase {
             Attach.pageSource();
             Attach.browserConsoleLogs();
             Attach.addVideo();
+        //    Selenide.closeWebDriver();
         }
 }
 
