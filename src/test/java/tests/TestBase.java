d0f51a89bd6bd7a3e827ec193ebedce5ac663f1f
Marina Konovalchik <marina200014@yandex.ru>
2023-04-13 23:13:42 +0300

    updated files

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,15 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RabotaByPage;
-
-import static com.codeborne.selenide.Selenide.open;
-import static config.WebDriverProvider.setConfig;
 
 
 public class TestBase {
@@ -18,30 +17,30 @@ public class TestBase {
 
         @BeforeAll
         static void beforeAll() {
-     //       Configuration.browser = System.getProperty("browser", "chrome");
-     //       Configuration.browserVersion = System.getProperty("browserVersion", "100");
-     //       Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-    //        Configuration.timeout = 10000;
-    //        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-    //        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+           Configuration.browser = System.getProperty("browser", "chrome");
+          Configuration.browserVersion = System.getProperty("browserVersion", "100");
+      Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.timeout = 10000;
+         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+          Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
-    //        DesiredCapabilities capabilities = new DesiredCapabilities();
-    //        capabilities.setCapability("enableVNC", true);
-    //        capabilities.setCapability("enableVideo", true);
-    //        Configuration.browserCapabilities = capabilities;
+         DesiredCapabilities capabilities = new DesiredCapabilities();
+         capabilities.setCapability("enableVNC", true);
+           capabilities.setCapability("enableVideo", true);
+           Configuration.browserCapabilities = capabilities;
 
 
-                setConfig();
+           //     setConfig();
             }
 
 
         @BeforeEach
         void addListener(){
-     //       SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
 
-                SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-                open("https://rabota.by");
+            //    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+            //    open("https://rabota.by");
         }
 
         @AfterEach
